e58c6d3d5ac70a3169912d33b72d29cf2c543e89
Mike Tery <miketery@gmail.com>
2024-04-29 15:34:50 -0400

    Disable guidelines upload until medical records uploaded first

--- frontend/components/guidelines-upload/index.tsx
+++ frontend/components/guidelines-upload/index.tsx
@@ -9,7 +9,7 @@ const SPINNER_TIMEOUT = 3000;
 
 export default function GuidelinesUpload() {
     const [ showSpinner, setShowSpinner ] = useState(false);
-    const { guidelinesFile, setGuidelinesFile } = useDashboard();
+    const { medicalRecord, guidelinesFile, setGuidelinesFile } = useDashboard();
 
     const handleClick = () => {
         setShowSpinner(true);
@@ -21,22 +21,27 @@ export default function GuidelinesUpload() {
 
     return(
         <div className="w-1/2 h-64 border border-4 border-gray-200 border-dashed rounded flex flex-row items-center justify-center">
-            {showSpinner ? <FaSpinner className="animate-spin text-gray-400" />: 
-            <button
-                className={classNames(
-                    "text-white font-medium py-2 px-4 rounded border border-2",
-                    guidelinesFile === null ? "bg-orange-500 border-orange-500" : "border-transparent text-green-600" 
+            {medicalRecord === null ? 
+                <div className="text-gray-500 text-center">
+                    <span>Upload Medical Record First</span>
+                </div> :
+                (showSpinner ? <FaSpinner className="animate-spin text-gray-400" /> : 
+                    <button
+                        className={classNames(
+                            "text-white font-medium py-2 px-4 rounded border border-2",
+                            guidelinesFile === null ? "bg-orange-500 border-orange-500" : "border-transparent text-green-600" 
+                        )}
+                        onClick={handleClick}
+                    >
+                        {guidelinesFile === null && (<span>Simulate Guidelines Upload</span>)}
+                        {guidelinesFile !== null && (
+                            <span className="text-green-600 flex flex-row gap-1 items-center">
+                                <FaCheck />
+                                <span>Guidelines File Uploaded</span>
+                            </span>
+                        )}
+                    </button>
                 )}
-                onClick={handleClick}
-            >
-                {guidelinesFile === null && (<span>Simulate Guidelines Upload</span>)}
-                {guidelinesFile !== null && (
-                    <span className="text-green-600 flex flex-row gap-1 items-center">
-                        <FaCheck />
-                        <span>Guidelines File Uploaded</span>
-                    </span>
-                )}
-            </button>}
         </div>
     )
 }
