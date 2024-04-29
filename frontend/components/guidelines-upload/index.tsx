37b33858fb4ea06a28b15903edfac6cd45561953
Mike Tery <miketery@gmail.com>
2024-04-29 15:12:10 -0400

    Added spinner for 3 seconds on guidelines and medical record upload components

--- frontend/components/guidelines-upload/index.tsx
+++ frontend/components/guidelines-upload/index.tsx
@@ -1,18 +1,27 @@
 "use client";
 
+import { useState } from 'react';
 import { useDashboard} from "@/context/dashboard-context";
 import classNames from "classnames";
-import { FaCheck } from "react-icons/fa";
+import { FaCheck, FaSpinner } from "react-icons/fa";
+
+const SPINNER_TIMEOUT = 3000;
 
 export default function GuidelinesUpload() {
+    const [ showSpinner, setShowSpinner ] = useState(false);
     const { guidelinesFile, setGuidelinesFile } = useDashboard();
 
     const handleClick = () => {
+        setShowSpinner(true);
         setGuidelinesFile({ url: "/assets/guidelines.pdf" });
+        setTimeout(() => {
+            setShowSpinner(false);
+        }, SPINNER_TIMEOUT);
     }
 
     return(
         <div className="w-1/2 h-64 border border-4 border-gray-200 border-dashed rounded flex flex-row items-center justify-center">
+            {showSpinner ? <FaSpinner className="animate-spin text-gray-400" />: 
             <button
                 className={classNames(
                     "text-white font-medium py-2 px-4 rounded border border-2",
@@ -27,7 +36,7 @@ export default function GuidelinesUpload() {
                         <span>Guidelines File Uploaded</span>
                     </span>
                 )}
-            </button>
+            </button>}
         </div>
     )
 }
--- frontend/components/medical-record-upload/index.tsx
+++ frontend/components/medical-record-upload/index.tsx
@@ -1,18 +1,27 @@
 "use client";
 
+import { useState } from 'react';
 import { useDashboard } from "@/context/dashboard-context";
 import classNames from "classnames";
-import { FaCheck } from "react-icons/fa";
+import { FaCheck, FaSpinner } from "react-icons/fa";
+
+const SPINNER_TIMEOUT = 3000;
 
 export default function MedicalRecordUpload() {
+    const [ showSpinner, setShowSpinner ] = useState(false);
     const { medicalRecord, setMedicalRecord } = useDashboard();
 
     const handleClick = () => {
+        setShowSpinner(true);
         setMedicalRecord({ url: "/assets/medical-record.pdf" });
+        setTimeout(() => {
+            setShowSpinner(false);
+        }, SPINNER_TIMEOUT);
     }
 
     return(
         <div className="w-1/2 h-64 border border-4 border-gray-200 border-dashed rounded flex flex-row items-center justify-center">
+            {showSpinner ? <FaSpinner className="animate-spin text-gray-400" />: 
             <button
                 className={classNames(
                     "text-white font-medium py-2 px-4 rounded border border-2",
@@ -27,7 +36,7 @@ export default function MedicalRecordUpload() {
                         <span>Medical Record Uploaded</span>
                     </span>
                 )}
-            </button>
+            </button>}
         </div>
     )
 }
