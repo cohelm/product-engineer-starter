0a569b9c806d7437e4f57de9f3d2e0c800c0dc27
Max <mmandia511@gmail.com>
2024-06-18 20:17:11 -0400

    fix: removing 'async' from <DashboardRoot />

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -6,28 +6,27 @@ import { useRouter } from "next/navigation";
 
 export const revalidate = 0;
 
-export default async function DashboardRoot() {
-	const router = useRouter();
-	const CASE_ID = "case_891a_6fbl_87d1_4326";
+export default function DashboardRoot() {
+    const router = useRouter();
+    const CASE_ID = "case_891a_6fbl_87d1_4326";
 
-	const handleContinue = () => {
-		router.push(`/dashboard/case/${CASE_ID}`)
-	}
+    const handleContinue = () => {
+        router.push(`/dashboard/case/${CASE_ID}`);
+    };
 
-	return (
-		<div className="w-full flex flex-col justify-center items-center h-screen">
-			<div className="w-full flex flex-row gap-2 items-center">
-				<MedicalRecordUpload />
-				<GuidelinesUpload />
-			</div>
-			<div className="w-full py-4 flex flex-row justify-center">
-				<button
-					className="bg-green-600 font-medium text-white py-2 px-4 rounded"
-					onClick={handleContinue}
-				>
-					Continue
-				</button>
-			</div>
-		</div>
-	)
+    return (
+        <div className="w-full flex flex-col justify-center items-center h-screen">
+            <div className="w-full flex flex-row gap-2 items-center">
+                <MedicalRecordUpload />
+                <GuidelinesUpload />
+            </div>
+            <div className="w-full py-4 flex flex-row justify-center">
+                <button
+                    className="bg-green-600 font-medium text-white py-2 px-4 rounded"
+                    onClick={handleContinue}>
+                    Continue
+                </button>
+            </div>
+        </div>
+    );
 }
