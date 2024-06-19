5c728e5717738f688cc017f2e721524babce0cbb
Max <mmandia511@gmail.com>
2024-06-18 20:09:46 -0400

    feat: added toaster from shadcn/ui

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,19 +1,21 @@
 import "@/styles/globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 interface IRootLayoutProps {
-	children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 export default function RootLayout(props: IRootLayoutProps) {
-	const { children } = props;
-	
-	return (
-		<html lang="en">
-			<head></head>
-			<body>
-				{children}
-				<div id="modal" />
-			</body>
-		</html>
-	)
+    const { children } = props;
+
+    return (
+        <html lang="en">
+            <head></head>
+            <body>
+                {children}
+                <div id="modal" />
+                <Toaster />
+            </body>
+        </html>
+    );
 }
