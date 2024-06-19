9e08a5e2a5362820af7eb0133c0f4c87083f3960
Max <mmandia511@gmail.com>
2024-06-18 20:12:23 -0400

    feat/style: guidelines file upload

--- frontend/components/guidelines-upload/index.tsx
+++ frontend/components/guidelines-upload/index.tsx
@@ -1,33 +1,80 @@
 "use client";
 
-import { useDashboard} from "@/context/dashboard-context";
+import { useDashboard } from "@/context/dashboard-context";
 import classNames from "classnames";
+import { useEffect, useState } from "react";
 import { FaCheck } from "react-icons/fa";
+import { Button } from "../ui/button";
+import { Loader2 } from "lucide-react";
+import { useToast } from "@/components/ui/use-toast";
 
 export default function GuidelinesUpload() {
-    const { guidelinesFile, setGuidelinesFile } = useDashboard();
+    const { isLoading, simulateUpload, guidelinesFile } = useGuidelinesUpload();
 
-    const handleClick = () => {
-        setGuidelinesFile({ url: "/assets/guidelines.pdf" });
-    }
+    const buttonClass = classNames({
+        "bg-green-500": guidelinesFile,
+        "hover:bg-green-600": guidelinesFile
+    });
 
-    return(
-        <div className="w-1/2 h-64 border border-4 border-gray-200 border-dashed rounded flex flex-row items-center justify-center">
-            <button
-                className={classNames(
-                    "text-white font-medium py-2 px-4 rounded border border-2",
-                    guidelinesFile === null ? "bg-orange-500 border-orange-500" : "border-transparent text-green-600" 
-                )}
-                onClick={handleClick}
-            >
-                {guidelinesFile === null && (<span>Simulate Guidelines Upload</span>)}
-                {guidelinesFile !== null && (
-                    <span className="text-green-600 flex flex-row gap-1 items-center">
-                        <FaCheck />
-                        <span>Guidelines File Uploaded</span>
-                    </span>
-                )}
-            </button>
+    const uploadButtonText = isLoading ? (
+        <>
+            <Loader2 className="w-4 h-4 mr-3 animate-spin" />
+            <span>Uploading...</span>
+        </>
+    ) : guidelinesFile ? (
+        <span className="flex flex-row gap-1 items-center">
+            <FaCheck />
+            <span>Guidelines File Uploaded</span>
+        </span>
+    ) : (
+        <span>Simulate Guidelines File Upload</span>
+    );
+
+    return (
+        <div className="w-1/2 h-64  border-4 border-gray-200 border-dashed rounded flex flex-row items-center justify-center">
+            <Button disabled={isLoading} className={buttonClass} onClick={simulateUpload}>
+                {uploadButtonText}
+            </Button>
         </div>
-    )
+    );
+}
+
+// hook to simulate guideline record upload
+function useGuidelinesUpload() {
+    const { toast } = useToast();
+    const { guidelinesFile, setGuidelinesFile, medicalRecord } = useDashboard();
+    const [isLoading, setIsLoading] = useState(false);
+
+    const simulateUpload = async () => {
+        // if medical record is not uploaded, show toast and return
+        if (!medicalRecord) {
+            toast({
+                title: "Failed to upload guidelines file",
+                description: "Please upload a medical record first"
+            });
+            return;
+        }
+
+        // if guidelines file record is already uploaded,
+        // reset guidelines file so a user cannot continue until the guidelines file is uploaded
+        if (guidelinesFile) {
+            setGuidelinesFile(null);
+        }
+
+        setIsLoading(true);
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+        setGuidelinesFile({ url: "/assets/guidelines.pdf" });
+    };
+
+    /*
+        Since setState is async, we need to wait for the state to update before we can set isLoading to false
+        Normally we would use a lib like react-query for real data fetching that has built-in loading states, caching, etc.
+    */
+    useEffect(() => {
+        if (guidelinesFile) {
+            setIsLoading(false);
+        }
+    }, [guidelinesFile]);
+
+    return { isLoading, simulateUpload, guidelinesFile };
 }
